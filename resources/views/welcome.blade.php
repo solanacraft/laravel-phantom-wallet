6570ae18111945a4582bf630229391d7d23a46de
movsumov <farid@otrium.com>
2022-01-12 18:54:31 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config("app.name") }}</title>
+    </head>
+    <body>
+        <div>
+            Hello world!
+        </div>
+    </body>
+</html>
