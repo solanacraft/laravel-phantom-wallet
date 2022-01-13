fd9a0f568bcdc04c77ec7bc68dd526b3167e4c88
movsumov <farid@otrium.com>
2022-01-13 17:52:04 +0100

    phantom login implemented

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,21 +8,42 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 
-    <title>Hello, world!</title>
+    <title>{{ config('app.name') }}</title>
 </head>
 <body>
 
-<div class="container">
-    <button type="button" class="btn btn-primary">Primary</button>
+<div class="container-fluid text-center">
+    <h1>{{ config('app.name') }}</h1>
+    <div class="d-flex justify-content-center">
+        <button id="login-button" onclick="phantomLogin()" class="btn btn-dark">Login with Phantom Wallet</button>
+    </div>
+
+    <div class="d-flex justify-content-center ">
+        <div class="public-key" style="display: none"></div>
+    </div>
 </div>
 
 <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="/js/jquery-3.6.0.min.js"></script>
 
 <script>
-    $(document).ready(function () {
-
-    });
+    async function phantomLogin() {
+        const isPhantomInstalled = window.solana && window.solana.isPhantom;
+        if (!isPhantomInstalled) {
+            alert("Phantom plugin is not detected!");
+        } else {
+            try {
+                const resp = await window.solana.connect();
+                console.log('Account: ' + resp.publicKey.toString());
+                $('#login-button').hide();
+                let publicKeyDiv = $('.public-key');
+                publicKeyDiv.html(resp.publicKey.toString());
+                publicKeyDiv.show();
+            } catch (err) {
+                console.log("User rejected request");
+            }
+        }
+    }
 </script>
 
 </body>
