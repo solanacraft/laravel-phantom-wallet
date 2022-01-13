4874aca43e807e4f913a10d5e90401b7f4be571a
movsumov <farid@otrium.com>
2022-01-13 17:10:47 +0100

    bootstrap working now

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 
     <title>Hello, world!</title>
 </head>
 <body>
-<h1>Hello, world!</h1>
+
+<div class="container">
+    <button type="button" class="btn btn-primary">Primary</button>
+</div>
+
 <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/js/jquery-3.6.0.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+
+    });
+</script>
+
 </body>
 </html>
