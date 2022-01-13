38af65621211d90c7cd1384ddd23193c12dbe526
movsumov <farid@otrium.com>
2022-01-13 15:49:17 +0100

    bootstrap installation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="/css/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<h1>Hello, world!</h1>
+<script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
