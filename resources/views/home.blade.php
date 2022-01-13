7f9d8c8f0909b30ed343d8cab0e1d348a9f5a78e
movsumov <farid@otrium.com>
2022-01-13 16:21:21 +0100

    bootstrap installed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="/css/bootstrap.min.css">
+    <link href="/bootstrap/css/bootstrap.min.css">
 
     <title>Hello, world!</title>
 </head>
 <body>
 <h1>Hello, world!</h1>
-<script src="/js/bootstrap.bundle.min.js"></script>
+<script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
